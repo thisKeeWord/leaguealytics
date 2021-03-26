d660e9a8d6c688fac34dd942e0af324316702f87
thisKeeWord <leonardwkee@gmail.com>
2021-03-25 18:41:25 -0700

    adding some basic styles to chart

--- client/components/Chart/index.tsx
+++ client/components/Chart/index.tsx
@@ -1,16 +1,44 @@
 import React, { FunctionComponent } from 'react';
-import { VictoryBar, VictoryChart, VictoryLabel } from 'victory';
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryClipContainer,
+  VictoryContainer,
+  VictoryLabel,
+  VictoryPortal,
+} from 'victory';
+import styled from 'styled-components';
 
 interface ChartProps {
   data: any;
   title: string;
 }
 
+const StyledChart = styled.div`
+  display: inline-block;
+  padding: 40px;
+  width: 50%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
 const Chart: FunctionComponent<ChartProps> = (props: ChartProps) => {
   return (
-    <svg viewBox='0 0 400 400' height='80%' width='50%'>
-      <VictoryChart standalone={false} domainPadding={10} height={400} width={400} horizontal>
+    <StyledChart>
+      <VictoryChart domainPadding={10} height={400} width={400} horizontal>
         <VictoryLabel text={props.title} y={20} />
+        <VictoryAxis
+          dependentAxis
+          standalone={false}
+          tickLabelComponent={<VictoryLabel renderInPortal={true} />}
+        />
+        <VictoryAxis
+          standalone={false}
+          tickLabelComponent={<VictoryLabel renderInPortal={true} />}
+        />
         <VictoryBar
           style={{
             data: { fill: '#c43a31' },
@@ -20,7 +48,7 @@ const Chart: FunctionComponent<ChartProps> = (props: ChartProps) => {
           labelComponent={<VictoryLabel dy={0} />}
         />
       </VictoryChart>
-    </svg>
+    </StyledChart>
   );
 };
 
