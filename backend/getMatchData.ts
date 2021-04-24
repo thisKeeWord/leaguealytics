622f738013b8099540bb4837f7408f1ecde8f9d2
thisKeeWord <leonardwkee@gmail.com>
2021-04-24 01:51:22 -0700

    add items, but has some edge cases not considered, may not be maintained

--- backend/getMatchData.ts
+++ backend/getMatchData.ts
@@ -22,7 +22,7 @@ export const getMatchData = async (req, res) => {
     // eslint-disable-next-line prefer-const
     let byTimeframe: Record<any, any>[] = [];
     // eslint-disable-next-line prefer-const
-    let participantFrames: Record<string, any> = {};
+    let participantFrames: Record<string, any>[] = [];
     for (let i = 0; i < matchTimelineData.info.frames.length; i++) {
       // eslint-disable-next-line prefer-const
       let frame = matchTimelineData.info.frames[i];
@@ -39,39 +39,33 @@ export const getMatchData = async (req, res) => {
           // eslint-disable-next-line max-len
           damageDoneToChampions: frame.participantFrames[key].damageStats.totalDamageDoneToChampions,
           damageTaken: frame.participantFrames[key].damageStats.totalDamageTaken,
+          items: (Boolean(participantFrames[key]) && participantFrames[key].items) || [],
+          assists: (Boolean(participantFrames[key]) && participantFrames[key].assists) || 0,
+          deaths: (Boolean(participantFrames[key]) && participantFrames[key].deaths) || 0,
+          kills: (Boolean(participantFrames[key]) && participantFrames[key].kills) || 0,
+          wardsPlaced: (Boolean(participantFrames[key]) && participantFrames[key].wardsPlaced) || 0,
+          // eslint-disable-next-line max-len
+          wardsPurchased: (Boolean(participantFrames[key]) && participantFrames[key].wardsPurchased) || 0,
+          wardsKilled: (Boolean(participantFrames[key]) && participantFrames[key].wardsKilled) || 0,
 
         };
       }
       for (let j = 0; j < frame.events.length; j++) {
         const {
           // eslint-disable-next-line max-len, no-unused-vars
-          type, creatorId, victimId, position, killerId, assistingParticipantIds, timestamp, itemId, participantId, beforeId, afterId,
+          type, creatorId, victimId, position, killerId, assistingParticipantIds, timestamp, itemId, participantId, beforeId, afterId, wardType,
         } = frame.events[j];
-
-        // console.log(participantFrames, 'participantFrames');
 
         if (type === 'CHAMPION_KILL') {
           events.push(frame.events[j]);
           if (assistingParticipantIds) {
             assistingParticipantIds.forEach((assistId) => {
-              if (!participantFrames[assistId].assists) {
-                participantFrames[assistId].assists = 1;
-              } else {
-                participantFrames[assistId].assists++;
-              }
+              participantFrames[assistId].assists++;
             });
           }
-          if (!participantFrames[victimId].deaths) {
-            participantFrames[victimId].deaths = 1;
-          } else {
-            participantFrames[victimId].deaths++;
-          }
+          participantFrames[victimId].deaths++;
           if (killerId !== 0) {
-            if (!participantFrames[killerId].kills) {
-              participantFrames[killerId].kills = 1;
-            } else {
-              participantFrames[killerId].kills++;
-            }
+            participantFrames[killerId].kills++;
           }
         }
 
@@ -79,71 +73,124 @@ export const getMatchData = async (req, res) => {
           events.push(frame.events[j]);
         }
         if (type === 'WARD_PLACED') {
-          if (!participantFrames[creatorId].wardsPlaced) {
-            participantFrames[creatorId].wardsPlaced = 1;
-          } else {
+          if (wardType !== 'UNDEFINED') {
             participantFrames[creatorId].wardsPlaced++;
           }
         }
         if (type === 'WARD_KILL') {
-          if (!participantFrames[killerId].wardsKilled) {
-            participantFrames[killerId].wardsKilled = 1;
-          } else {
+          if (wardType !== 'UNDEFINED') {
             participantFrames[killerId].wardsKilled++;
           }
         }
-        // if (['ITEM_SOLD', 'ITEM_DESTROYED'].includes(type)) {
-        //   // eslint-disable-next-line max-len
-        //   if (Array.isArray(participantFrames[participantId].items)) {
-        // eslint-disable-next-line max-len
-        //     participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1);
-        //   }
-        // }
-        // if (type === 'ITEM_PURCHASED') {
-        //   if (!participantFrames[participantId].items) {
-        //     participantFrames[participantId].items = [itemId];
-        //   } else {
-        //     participantFrames[participantId].items.push(itemId);
-        //   }
-        //   // control ward
-        //   if (itemId === 2055) {
-        //     // eslint-disable-next-line max-len
-        //     if (!participantFrames[participantId].wardsPurchased) {
-        //       participantFrames[participantId].wardsPurchased = 1;
-        //     } else {
-        //       participantFrames[participantId].wardsPurchased++;
-        //     }
-        //   }
-        // }
-        // if (type === 'ITEM_UNDO') {
-        //   if (Array.isArray(afterId)) {
-        //   // eslint-disable-next-line max-len
-        // eslint-disable-next-line max-len
-        //     participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1, ...afterId);
-        //   } else if (afterId) {
-        //     // eslint-disable-next-line max-len
-        // eslint-disable-next-line max-len
-        //     participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1, afterId);
-        //   } else {
-        //     // eslint-disable-next-line max-len
-        // eslint-disable-next-line max-len
-        //     participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1);
-        //   }
-        // }
+
+        // ITEM HANDLING MAY REMOVED AS IT REQUIRES POTENTIAL FREQUENT UPDATES
+        if (type === 'ITEM_SOLD') {
+          // eslint-disable-next-line max-len
+          participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1);
+        }
+
+        // ITEM HANDLING MAY REMOVED AS IT REQUIRES POTENTIAL FREQUENT UPDATES
+        /* special cases for later since they are, with the exception of
+        ** stopwatch and broken stopwatch, items not bought:
+        ** - biscuit (2410)
+        ** - commencing stopwatch (2419)
+        ** - stopwatch (2420)
+        ** - broken stopwatch (2421)
+        ** - minion dematerializer (2403)
+        ** - slightly magical footwear (2422)
+        ** - your cut - pyke's ultimate (3400)
+        ** - eye of the herald (3513)
+        ** items that automatically transform without further purchases:
+        ** - archangel's staff (3003) -> 3040
+        ** - manamune (3004) -> 3042
+        ** - spellthief's edge (3850) -> 3851
+        ** - frostfang (3851) -> 3853
+        ** - steel shoulderguards (3854) -> 3855
+        ** - runesteel spaulders (3855) -> 3857
+        ** - relic shield (3858) -> 3859
+        ** - targon's buckler (3859) -> 3860
+        ** - spectral sickle (3862) -> 3863
+        ** - harrowing crescent (3863) -> 3864
+        ** if item is destroyed or sold and is in inventory
+        */
+        if (type === 'ITEM_DESTROYED') {
+          if (itemId === 3003) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3040);
+          } else if (itemId === 3004) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3042);
+          } else if (itemId === 3850) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3851);
+          } else if (itemId === 3851) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3853);
+          } else if (itemId === 3854) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3855);
+          } else if (itemId === 3855) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3857);
+          } else if (itemId === 3858) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3859);
+          } else if (itemId === 3859) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3860);
+          } else if (itemId === 3862) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3863);
+          } else if (itemId === 3863) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1, 3864);
+          } else {
+          // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(itemId), 1);
+          }
+        }
+        // ITEM HANDLING MAY REMOVED AS IT REQUIRES POTENTIAL FREQUENT UPDATES
+        if (type === 'ITEM_PURCHASED') {
+          // eslint-disable-next-line max-len
+          participantFrames[participantId].items.push(itemId);
+          // control ward
+          if (itemId === 2055) {
+            participantFrames[participantId].wardsPurchased++;
+          }
+        }
+        // ITEM HANDLING MAY REMOVED AS IT REQUIRES POTENTIAL FREQUENT UPDATES
+        if (type === 'ITEM_UNDO') {
+          if (Array.isArray(afterId)) {
+          // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1, ...afterId);
+          } else if (afterId) {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1, afterId);
+          } else {
+            // eslint-disable-next-line max-len
+            participantFrames[participantId].items.splice(participantFrames[participantId].items.lastIndexOf(beforeId), 1);
+          }
+        }
       }
 
       // eslint-disable-next-line max-len
-      const modifiedParticipantFrames = Object.keys(participantFrames).map((key) => participantFrames[key]);
+      const filteredParticipantFrames = participantFrames.filter((participantFrame) => !!participantFrame);
+      // ITEM HANDLING MAY REMOVED AS IT REQUIRES POTENTIAL FREQUENT UPDATES
+      const updatedParticipantFrames = filteredParticipantFrames.map((filteredFrame) => {
+        // not sure why the 'items' array is always being set to the last one
+        filteredFrame.itemSet = { ...filteredFrame.items };
 
+        return filteredFrame;
+      });
+
+      // console.log(util.inspect(filteredParticipantFrames), { showHidden: false, depth: null });
       // eslint-disable-next-line max-len
-      byTimeframe.push({ timestamp: frame.timestamp, events, participantFrames: modifiedParticipantFrames });
+      byTimeframe.push({ timestamp: frame.timestamp, events, participantFrames: updatedParticipantFrames });
     }
-
-    // console.log(byTimeframe.length, matchTimelineData.info.frames.length);
 
     await firestoreMatchDoc.set({ byTimeframe }, { merge: true });
 
-    res.send({ ...firestoreMatchData, byTimeframe: Object.values(byTimeframe) });
+    res.send({ ...firestoreMatchData, byTimeframe });
   } catch (error) {
     res.send({ error: error.message });
   }
