dd442ccc6c70e76bb8a30637d61b4422c0918eb5
Leonard <leonardwkee@gmail.com>
2021-06-08 21:52:41 -0700

    add ward placed and ward kill stat, styles

--- client/components/Match/MatchEvents/styles.ts
+++ client/components/Match/MatchEvents/styles.ts
@@ -3,63 +3,69 @@ import styled from 'styled-components';
 export const StyledMatchEvents = styled.div`
   width: 50%;
   padding: 0 10px 10px 10px;
-  max-height: 486px;
-  overflow-y: scroll;
-
+  
   h4 {
     text-align: center;
     margin-top: 10px;
   }
-
+  
   .events {
     background-color: rgba(0,0,0,0.1);
     border: 1px solid rgba(84,84,84,0.3);
+    height: 486px;
+    max-height: 486px;
     padding: 2px;
+    overflow-y: scroll;
 
     .champion-kills {
-      display: flex;
-      justify-content: space-evenly;
+      width: 245px;
       padding: 5px;
+      margin: 0 auto;
 
-      .champion-killer {
-        height: 40px;
+      .team-kill {
+        display: flex;
+        justify-content: space-between;
 
-        &.blue {
-          width: 40px;
-
-          img {
-            border: 2px solid #2747e8;
-          }
-        }
-
-        &.red {
-          width: 40px;
-
-          img {
-            border: 2px solid #cb2124;
-          }
-        }
-
-        &.user {
-          width: 40px;
-
-          img {
-            border: 2px solid purple;
-          }
-        }
-
-        img {
+        .champion-killer {
           height: 40px;
-          width: 40px;
+
+          &.blue {
+            width: 40px;
+
+            img {
+              border: 2px solid #2747e8;
+            }
+          }
+
+          &.red {
+            width: 40px;
+
+            img {
+              border: 2px solid #cb2124;
+            }
+          }
+
+          &.user {
+            width: 40px;
+
+            img {
+              border: 2px solid purple;
+            }
+          }
+
+          img {
+            height: 40px;
+            width: 40px;
+          }
         }
-      }
 
-      .desc-icon {
-        padding: 7px;
+        .desc-icon {
+          padding: 7px;
 
-        img {
-          height: 24px;
-          width: 24px;
+          img {
+            height: 24px;
+            width: 24px;
+          }
         }
       }
 
@@ -125,9 +131,54 @@ export const StyledMatchEvents = styled.div`
         }
       }
     }
+
+    .ward-event {
+      width: 245px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 5px;
+      margin: 0 auto;
+
+      span {
+        padding: 0 5px;
+      }
+
+      &.blue {
+        img.champion {
+          width: 40px;
+          border: 2px solid #2747e8;
+        }
+      }
+
+      &.red {
+        img.champion {
+          width: 40px;
+          border: 2px solid #cb2124;
+        }
+      }
+
+      &.user {
+        img.champion {
+          width: 40px;
+          border: 2px solid purple;
+        }
+      }
+
+      img {
+        height: 40px;
+        width: 40px;
+      }
+    }
   }
 
   @media screen and (max-width: 1000px) {
+    width: auto;
     margin: 0 auto;
+
+    .events {
+      height: auto;
+      width: 486px;
+    }
   }
 `;
