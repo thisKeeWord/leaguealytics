6cd28e6d2fecaebceb10be777c24582a1c0a9f6d
Leonard <leonardwkee@gmail.com>
2021-06-24 20:26:12 -0700

    add loading spinner

---
+++ client/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import { StyledLoading } from './styles';
+
+const Loading: FunctionComponent = () => (
+  <StyledLoading />
+);
+
+export default Loading;
