fdb79a1fe9b56ad6fbeccf32d0c9928121a59836
Leonard <leonardwkee@gmail.com>
2021-07-20 21:11:09 -0700

    fixed chart style on mobile

--- client/components/Chart/styles.ts
+++ client/components/Chart/styles.ts
@@ -5,7 +5,7 @@ export const StyledChart = styled.div`
   display: inline-block;
   padding: 10px;
   width: 50%;
-  height: 40%;
+  height: 450px;
 
   .sort {
     position: absolute;
@@ -14,7 +14,7 @@ export const StyledChart = styled.div`
     z-index: 1;
   }
 
-  @media (max-width: 1000px) {
+  @media screen and (max-width: 1000px) {
     width: 100%;
   }
 `;
