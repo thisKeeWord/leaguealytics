3e94a9a2777d73c47634d1aaffbeac5e431a1d32
Leonard <leonardwkee@gmail.com>
2021-08-30 22:12:04 -0700

    remove bundle from gitignore

--- client/state/actions/getMatchTimeline.ts
+++ client/state/actions/getMatchTimeline.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { AppThunk } from '../index';
 import { loadMatchFailure, loadMatchSuccess, setMatchFetching } from '../reducers/match';
 
