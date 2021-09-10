3778be4179b40527671a9f301384aa664c58b421
Leonard <leonardwkee@gmail.com>
2021-09-10 11:54:59 -0700

    switch webpack to prod

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
     filename: 'bundle.js',
   },
   devtool: 'eval-source-map',
-  mode: 'development',
-  // mode: 'production',
+  // mode: 'development',
+  mode: 'production',
   devServer: {
     // Required for Docker to work with dev server
     // host: "0.0.0.0",
